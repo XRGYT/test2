fecdc1485d9b0b3a7ac0d0de8f8409d98ad0fc36
XRGYT <96961734+XRGYT@users.noreply.github.com>
2023-05-29 19:02:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,47 @@
-const newsContainer = document.getElementById('newsContainer');
-const adminLoginForm = document.getElementById('adminLoginForm');
-const newsForm = document.getElementById('newsForm');
-const articleForm = document.getElementById('articleForm');
-
-adminLoginForm.addEventListener('submit', e => {
-  e.preventDefault();
-
-  const username = e.target.elements.username.value;
-  const password = e.target.elements.password.value;
-
-  // Perform authentication
-  authenticateAdmin(username, password)
-    .then(() => {
-      adminLoginForm.style.display = 'none';
-      newsForm.style.display = 'block';
-    })
-    .catch(error => {
-      console.error('Authentication error:', error);
-    });
-});
-
-articleForm.addEventListener('submit', e => {
-  e.preventDefault();
-
-  const title = e.target.elements.title.value;
-  const content = e.target.elements.content.value;
-  const image = e.target.elements.image.files[0];
-
-  // Display the new article immediately
-  displayNewArticle(title, content, image);
-
-  e.target.reset();
-});
-
-function authenticateAdmin(username, password) {
-  // Perform authentication logic (e.g., send request to server for verification)
-  return new Promise((resolve, reject) => {
-    // Simulating successful authentication for demonstration purposes
-    if (username === 'admin' && password === 'password') {
-      resolve();
-    } else {
-      reject('Invalid credentials');
-    }
-  });
-}
-
-function displayNewArticle(title, content, image) {
-  const articleElement = document.createElement('div');
-  articleElement.classList.add('article');
-  
-  const titleElement = document.createElement('h2');
-  titleElement.textContent = title;
-  
-  const contentElement = document.createElement('p');
-  contentElement.textContent = content;
-  
-  articleElement.appendChild(titleElement);
-  articleElement.appendChild(contentElement);
-  
-  if (image) {
-    const imageElement = document.createElement('img');
-    imageElement.src = URL.createObjectURL(image);
-    articleElement.appendChild(imageElement);
+// Sample data for articles
+const articles = [
+  {
+    title: 'Recent News',
+    content: 'We are developing this site to provide un-biased news. we are not funded by any government or anyone and we wish to provide legitimate information.',
+    image: 'new.png'
+  },
+  {
+    title: 'News Coming Soon',
+    content: '',
+    image: ''
   }
-  
-  newsContainer.prepend(articleElement);
+];
+
+// Function to add articles to the HTML
+function displayArticles() {
+  const articlesContainer = document.getElementById('articles');
+
+  // Loop through the articles data
+  for (let i = 0; i < articles.length; i++) {
+    const article = articles[i];
+
+    // Create article element
+    const articleElement = document.createElement('article');
+
+    // Create title element
+    const titleElement = document.createElement('h2');
+    titleElement.textContent = article.title;
+    articleElement.appendChild(titleElement);
+
+    // Create content element
+    const contentElement = document.createElement('p');
+    contentElement.textContent = article.content;
+    articleElement.appendChild(contentElement);
+
+    // Create image element
+    const imageElement = document.createElement('img');
+    imageElement.src = article.image;
+    articleElement.appendChild(imageElement);
+
+    // Append article element to articles container
+    articlesContainer.appendChild(articleElement);
+  }
 }
+
+// Call the displayArticles function when the page loads
+window.addEventListener('DOMContentLoaded', displayArticles);
